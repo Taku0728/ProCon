e8f80afc3cf1c31438b2feff6dfb95833547b6d0
Taku0728 <38447757+Taku0728@users.noreply.github.com>
2018-05-01 17:15:13 +0900

    5/1

---
+++ CPP/AGC/AGC001-A/Source.cpp
@@ -0,0 +1,20 @@
+#include <cstdio>
+using namespace std;
+
+int N;
+char line[402];
+int L[201];
+
+void input() {
+	scanf_s("%d", &N);
+	scanf_s("%s", &line, 402);
+	for (int i(0); i < 2*N; ++i) {
+		
+	}
+}
+
+int main() {
+	input();
+	printf("%d", L[0]);
+	return 0;
+}
